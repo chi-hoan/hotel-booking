c6206ec72368cb898a8496b46a09947d6ca7523f
hoan1 <hoanlechi06@email.com>
2026-03-02 20:53:27 +0700

    update frontend with backend for booking

--- client/src/components/RecommendedHotels.jsx
+++ client/src/components/RecommendedHotels.jsx
@@ -9,20 +9,24 @@ const RecommendedHotels = () => {
     const [recommended, setRecommended] = useState([]);
 
     const filterHotels = () => {
-        const filteredHotels = rooms.slice().filter( room => searchedCities.includes(room.hotel.city));
+        if (searchedCities.length === 0) {
+            setRecommended(rooms.slice(0,4));
+        } else {
+            const filteredHotels = rooms.slice().filter( room => searchedCities.includes(room.hotel.city));
         setRecommended(filteredHotels);
+        }
     }
 
     useEffect(()=>{
-        filterHotels()
+        filterHotels();
     },[rooms, searchedCities])
 
-    return recommended.length < 0 && (
-    <div className='flex flex-col items-center px-6 md:px16 lg:px-24 bg-slate-50 py-20'>
+    return recommended.length > 0 && (
+    <div className='flex flex-col items-center px-6 md:px-16 lg:px-24 bg-slate-50 py-20'>
 
         <Title title='Recommended Hotels' subTitle='Discover our handpicked selection of exceptional properties around the world, offering unparalleled luxury and unforgettable experiences.' />
         
-        <div className='flex flex-grap items-center justify-center gap-6 mt-20'>
+        <div className='flex flex-wrap items-center justify-center gap-6 mt-20'>
             {recommended.slice(0, 4).map((room, index)=> (
                 <HotelCard key={room._id} room={room} index={index}/>
             ))}
