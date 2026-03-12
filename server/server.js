93ea89ecf7f04f8844dbb955ea9f4863aaf3c026
hoan1 <hoanlechi06@email.com>
2026-03-12 10:47:42 +0700

    fix file

--- server/server.js
+++ server/server.js
@@ -34,6 +34,7 @@ app.use('/api/rooms', roomRouter);
 app.use('/api/bookings', bookingRouter);
 
 
-const PORT = process.env.PORT || 3000
+//const PORT = process.env.PORT || 3000
+//app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`));
 
-app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`));
+export default app;
